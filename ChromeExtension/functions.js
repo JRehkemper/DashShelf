10bf4a64e2235deb2b893025882ba3baeae3bedb
JadBlackstone <64414701+JadBlackstone@users.noreply.github.com>
2022-07-19 08:55:49 +0200

    add support for unsorted Bookmarks; Add Bookmark Icon with CSS

--- ChromeExtension/functions.js
+++ ChromeExtension/functions.js
@@ -8,19 +8,31 @@ chrome.tabs.query({active: true}, function(tabs) {
 })
 
 function main() {
-    console.log("Func main")
-    var bookmarksList = bookmarksFile[0]["children"][0]["children"]
+    var bookmarkArr = bookmarksFile[0]["children"][0]["children"]
+    var bookmarksList = []
+    var unsortedBookmarks = []
+    for(var i = 0; i < bookmarkArr.length; i++) {
+        if(typeof bookmarkArr[i]["children"] != 'undefined') {
+            bookmarksList.push(bookmarkArr[i])
+        }
+        else {
+            unsortedBookmarks.push(bookmarkArr[i])
+        }
+    }
     var numberOfDirs = bookmarksList.length
     for (let i = 0; i < numberOfDirs; i++) {
         addElements(bookmarksList[i], i);
     }
+    document.getElementById("bookmarks").innerHTML += `
+        <div class="directory" id="directory-unsorted">
+            <h2 class="directory-title">Unsorted Bookmarks</h2>
+        </div>`
+    addUnsortedElements(unsortedBookmarks)
 }
 
 function addElements(elements, elementIndex) {
-    console.log(elements["title"])
     var children = elements["children"]
     
-
     // Add HTML Code
     document.getElementById("bookmarks").innerHTML += `
     <div class="directory" id="directory${elementIndex}">
@@ -28,12 +40,12 @@ function addElements(elements, elementIndex) {
     </div>
     `
 
-    console.log(children[0])
     for (let i = 0; i < children.length; i++){
         document.getElementById("directory"+elementIndex).innerHTML += `
             <div class="card">
                 <div class="thumbnail-div">
-                    <img class="thumbnail" src="https://jrehkemper.de/img/favicon.svg">
+                    <!--<img class="thumbnail" src="https://jrehkemper.de/img/favicon.svg">-->
+                    <div class="bookmark-icon"></div>
                 </div>
                 <div class="card-title-div">
                     <a class="card-title" href=${children[i]["url"]} target="_blank">${children[i]["title"]}</a>    
@@ -41,5 +53,20 @@ function addElements(elements, elementIndex) {
             </div>
         `
     }
-    console.log(document.getElementById("directory"+elementIndex))
+}
+
+function addUnsortedElements(elements) {
+    for (var i = 0; i < elements.length; i++) {
+        document.getElementById("directory-unsorted").innerHTML += `
+            <div class="card">
+                <div class="thumbnail-div">
+                    <!--<img class="thumbnail" src="https://jrehkemper.de/img/favicon.svg">-->
+                    <div class="bookmark-icon"></div>
+                </div>
+                <div class="card-title-div">
+                    <a class="card-title" href="${elements[i]["url"]}" target="_blank">${elements[i]["title"]}</a>
+                </div>
+            </div>
+        `
+    }
 }
