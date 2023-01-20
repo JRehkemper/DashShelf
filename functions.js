03e0bec9eed17ce3ae87a8fd8cd2d589d457caf4
JadBlackstone <64414701+JadBlackstone@users.noreply.github.com>
2023-01-20 12:02:24 +0100

    added comments

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
+
+// Read Bookmarks-File
 var bookmarksFile
 chrome.bookmarks.getTree(function(tree) {
     bookmarksFile = tree;
@@ -13,6 +15,7 @@ chrome.tabs.query({active: true}, function(tabs) {
 })
 
 function main() {
+    // Create Date Prompt
     var date = new Date()
     var day = date.getDate()
     var month = date.getMonth()
@@ -82,42 +85,60 @@ function main() {
             monthString = "December"
             break;
     }
+    // Display Date-Prompt to HTML
     var dateString = dayOfWeek+" "+day+". "+monthString+" "+year
     document.getElementById("date").innerHTML+= dateString
+
+    // Add Event-Listener to Size-Slider
     document.getElementById("save-width").addEventListener('click', saveWidth)
+    // Read old width Value from storage and set Display width
     try {
+        // Read Value from storage
         chrome.storage.sync.get(["width"], function(result) {
             console.log('Width loaded is ' + result.width);
             const root = document.documentElement
+            // Set Css Variable "--width" to saved width
             root.style.setProperty("--width", result.width)
+            // Set Size-slider to saved position
             document.getElementById("slider").value = result.width.replace("px", "")
         });
     }
+    // Catch if saved width cant be loaded
     catch {
+        // Log error and set default value
         console.log("Couldn't load old width,")
         document.getElementById("slider").value = "350px"
     }
+    // make Slider functional -> listen to changes and adjust css variable "--width"
     const slider = document.getElementById("slider");
     slider.addEventListener("input", function(e) {
         const root = document.documentElement
         root.style.setProperty("--width", e.target.value+"px")
     })
+
+    // Create variables to process Bookmark File
     console.log(bookmarksFile)
     var bookmarkArr = bookmarksFile[0]["children"][0]["children"]
     var bookmarksList = []
     var unsortedBookmarks = []
+    // iterate over bookmarks and separate Folders from unsorted Links
     for(var i = 0; i < bookmarkArr.length; i++) {
         if(typeof bookmarkArr[i]["children"] != 'undefined') {
+            // Folders
             bookmarksList.push(bookmarkArr[i])
         }
         else {
+            // unsorted Links
             unsortedBookmarks.push(bookmarkArr[i])
         }
     }
+
+    // add folders to html
     var numberOfDirs = bookmarksList.length
     for (let i = 0; i < numberOfDirs; i++) {
         addElements(bookmarksList[i], i);
     }
+    // add unsorted Links to html
     if(unsortedBookmarks.length > 0) {
         document.getElementById("bookmarks").innerHTML += `
             <div class="directory" id="directory-unsorted">
@@ -128,8 +149,10 @@ function main() {
 }
 
 function saveWidth() {
+    // read slider value
     width = document.getElementById("slider").value+"px"
     //console.log(width)
+    // save with to chrome storage
     chrome.storage.sync.set({"width": width}, function() {
         console.log('Width is set to ' + width);
     });
@@ -140,14 +163,16 @@ function addElements(elements, elementIndex) {
     var children = elements["children"]
 
     
-    // Add HTML Code
+    // Add HTML Code for folder
     document.getElementById("bookmarks").innerHTML += `
     <div class="directory" id="directory${elementIndex}">
     <h2 class="directory-title">${elements["title"]}</h2>
     </div>
     `
     
+    // add entry for every child of the folder and genarate html
     for (let i = 0; i < children.length; i++){
+        // let chrome read the favicon
         var favicon = `chrome-extension://${chrome.runtime.id}/_favicon/?pageUrl=${encodeURIComponent(children[i]["url"])}&size=32`;
         document.getElementById("directory"+elementIndex).innerHTML += `
             <a class="card-title" href=${children[i]["url"]} target="_blank">    
@@ -165,6 +190,7 @@ function addElements(elements, elementIndex) {
     }
 }
 
+// add entries for unsorted links and generate html
 function addUnsortedElements(elements) {
     for (var i = 0; i < elements.length; i++) {
         document.getElementById("directory-unsorted").innerHTML += `
